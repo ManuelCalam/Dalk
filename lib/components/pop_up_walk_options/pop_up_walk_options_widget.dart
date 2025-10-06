5332d74421ac626b8db492693c262f3f25d36bde
ManuelCalam <jmanuelc369@gmail.com>
2025-10-06 16:22:49 -0600

    Fix

--- lib/components/pop_up_walk_options/pop_up_walk_options_widget.dart
+++ lib/components/pop_up_walk_options/pop_up_walk_options_widget.dart
@@ -1,10 +1,8 @@
 import 'package:dalk/backend/supabase/supabase.dart';
-
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import 'dart:ui';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
