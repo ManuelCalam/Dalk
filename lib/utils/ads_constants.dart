5faf678dda35860c13f7e3151c7b5faf58afe851
ManuelCalam <jmanuelc369@gmail.com>
2025-11-19 01:37:23 -0600

    Ads permissions for google play console

--- lib/utils/ads_constants.dart
+++ lib/utils/ads_constants.dart
@@ -1,6 +1,15 @@
-const String bannerHomeId = 'ca-app-pub-3940256099942544/6300978111'; 
-const String bannerPetLisId = 'ca-app-pub-3940256099942544/6300978111';
-const String bannerWalksRecordId = 'ca-app-pub-3940256099942544/6300978111';
-const String bannerWalksListId = 'ca-app-pub-3940256099942544/6300978111';
-const String bannerFAQId = 'ca-app-pub-3940256099942544/6300978111';
-const String bannerWalkersList = 'ca-app-pub-3940256099942544/6300978111';
+//Test
+// const String bannerHomeId = 'ca-app-pub-3940256099942544/6300978111'; 
+// const String bannerPetLisId = 'ca-app-pub-3940256099942544/6300978111';
+// const String bannerWalksRecordId = 'ca-app-pub-3940256099942544/6300978111';
+// const String bannerWalksListId = 'ca-app-pub-3940256099942544/6300978111';
+// const String bannerFAQId = 'ca-app-pub-3940256099942544/6300978111';
+// const String bannerWalkersList = 'ca-app-pub-3940256099942544/6300978111';
+
+//Producción
+const String bannerHomeId = 'ca-app-pub-7465142412381768/6256971525'; 
+const String bannerPetLisId = 'ca-app-pub-7465142412381768/5598495976';
+const String bannerWalksRecordId = 'ca-app-pub-7465142412381768/5038316675';
+const String bannerWalksListId = 'ca-app-pub-7465142412381768/4331029223';
+const String bannerFAQId = 'ca-app-pub-7465142412381768/1126281384';
+const String bannerWalkersList = 'ca-app-pub-7465142412381768/2972332634';
